9d86048578cb0ebc9a1462d975ded9edc9fe433d
PurzaVlad <vpurza@gmail.com>
2024-06-12 13:41:55 +0300

    Muie sa sugi pula 2

---
+++ MihaiEAici/MihaiEAici/MihaiEAici/MihaiEAiciApp.swift
@@ -0,0 +1,17 @@
+//
+//  MihaiEAiciApp.swift
+//  MihaiEAici
+//
+//  Created by Andra Purza on 12.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct MihaiEAiciApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
